95ac7a15bc62a0d3fd2b9a26f524118e62145a58
rajasekhar pidikiti <rajasekhar.pidikitiios@gmail.com>
2020-02-18 16:30:33 +0530

    Initial Commit

---
+++ Duzy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Duzy
+//
+//  Created by Rajshekar on 18/02/20.
+//  Copyright © 2020 Sourcebits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
